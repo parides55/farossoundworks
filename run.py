11161c7bdd13c89eccf7ab201ab60106361037ee
Andreas Parides <parides5@hotmail.com>
2026-08-04 11:10:04 +0300

    add notes for the if __name__ block

--- run.py
+++ run.py
@@ -8,3 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# ensures your Flask dev server only starts when you run the file directly, not when it's imported elsewhere.
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 5000)), 
+        debug=True # turn off debug mode in production for security reasons
+    )
