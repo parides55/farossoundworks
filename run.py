fe9bef0a9227dc6c25b0e44fbb1461c5b598aaa0
Andreas Parides <parides5@hotmail.com>
2026-08-16 21:54:54 +0300

    change port and TLS and SSl settings

--- run.py
+++ run.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 
 # Email setup settings
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 25
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('EMAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
 
