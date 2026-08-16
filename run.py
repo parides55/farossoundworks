3c74dc811a6c4d5cf454ec60488b26f482e969ee
Andreas Parides <parides5@hotmail.com>
2026-08-16 21:32:01 +0300

    change port according to flask-mail docs

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 # Email setup settings
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 25
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('EMAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
