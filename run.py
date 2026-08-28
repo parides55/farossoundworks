ee9983c3cf906eeb08fcf538b5b144dc68bc92a8
Andreas Parides <parides5@hotmail.com>
2026-08-28 13:04:34 +0300

    create route to the about-us.html

--- run.py
+++ run.py
@@ -66,6 +66,11 @@ def services():
     return render_template('services.html')
 
 
+@app.route('/founder')
+def about_us():
+    return render_template('about-us.html')
+
+
 @app.route('/contact-us')
 def contact_us():   
     return render_template('contact-us.html')
